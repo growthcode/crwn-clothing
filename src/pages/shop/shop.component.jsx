dac7a4fd4c7ab6d45a5f29379f9a7d2cd5d1fb08
growthcode <growthcode@gmail.com>
2020-02-08 13:31:18 -0800

    add header component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 export class ShopPage extends Component {
   constructor(props) {
