2626faf2ec2dca6a96dcede77ce5bb1ed2c5e5ad
growthcode <growthcode@gmail.com>
2020-04-04 17:05:54 -0700

    26 - end - finish converting all components over to styled components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
+
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
-import './sign-in.styles.scss'
+import {
+  SignInContainer,
+  SignInTitle,
+  ButtonsBarContainer
+} from './sign-in.styles';
 
-export default class SignIn extends React.Component {
+class SignIn extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       email: '',
-      password: '',
-    }
+      password: ''
+    };
   }
 
-  handleSubmit = async(event) => {
+  handleSubmit = async event => {
     event.preventDefault();
+
     const { email, password } = this.state;
 
     try {
@@ -25,20 +32,20 @@ export default class SignIn extends React.Component {
     } catch (error) {
       console.log(error);
     }
-
-    this.setState({ email: '', password: '' })
   };
 
-  handleChange = (event) => {
+  handleChange = event => {
     const { value, name } = event.target;
+
     this.setState({ [name]: value });
   };
 
   render() {
     return (
-      <div className='sign-in'>
-        <h2>I already have an account</h2>
+      <SignInContainer>
+        <SignInTitle>I already have an account</SignInTitle>
         <span>Sign in with your email and password</span>
+
         <form onSubmit={this.handleSubmit}>
           <FormInput
             name='email'
@@ -56,14 +63,16 @@ export default class SignIn extends React.Component {
             label='password'
             required
           />
-          <div className='buttons'>
-            <CustomButton type='submit'>Sign In</CustomButton>
+          <ButtonsBarContainer>
+            <CustomButton type='submit'> Sign in </CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
               Sign in with Google
             </CustomButton>
-          </div>
+          </ButtonsBarContainer>
         </form>
-      </div>
+      </SignInContainer>
     );
   }
 }
+
+export default SignIn;
