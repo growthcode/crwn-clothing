9085155e58375430a4a5bc40771f1b3a67827451
growthcode <growthcode@gmail.com>
2020-05-10 12:54:21 -0700

    Lesson 18: section 28
    https://github.com/ZhangMYihua/lesson-28

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -7,7 +7,7 @@ const StripeCheckoutButton = ({ price }) => {
 
   const onToken = token => {
     console.log(token);
-    alert('Payment Successful');
+    alert('Payment Succesful!');
   };
 
   return (
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [];
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
-export const store = createStore(
-  rootReducer,
-  composeWithDevTools(
-    applyMiddleware(...middlewares)
-  ),
-);
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
 
